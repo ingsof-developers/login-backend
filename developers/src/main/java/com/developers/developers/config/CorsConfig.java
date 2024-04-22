4caf0dec2e1dcc3707fe9030656c6b697c0f2368
Jair0305 <chamaco_03@hotmail.es>
2024-04-22 04:25:32 -0600

    added cors configuration

---
+++ developers/src/main/java/com/developers/developers/config/CorsConfig.java
@@ -0,0 +1,21 @@
+package com.developers.developers.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig  {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer()
+    {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(org.springframework.web.servlet.config.annotation.CorsRegistry registry) {
+                registry.addMapping("/**").allowedMethods("*").allowedOrigins("*");
+            }
+        };
+    }
+
+}
